4b677811d13176ba7a39b1c1b78e6e2a9143c358
postalove <55823600+postalove@users.noreply.github.com>
2024-03-03 08:14:00 +0000

    .

--- main.py
+++ main.py
@@ -19,39 +19,25 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 import interactions
 # Use the following method to import the internal module in the current same directory
-from . import internal_t
+
 
 '''
 Replace the ModuleName with any name you'd like
 '''
 class ModuleName(interactions.Extension):
     module_base: interactions.SlashCommand = interactions.SlashCommand(
-        name="replace_your_command_base_here",
-        description="Replace here for the base command descriptions"
+        name="back",
+        description="回"
     )
     module_group: interactions.SlashCommand = module_base.group(
-        name="replace_your_command_group_here",
-        description="Replace here for the group command descriptions"
+        name="to",
+        description="到"
     )
 
-    @module_group.subcommand("ping", sub_cmd_description="Replace the description of this command")
-    @interactions.slash_option(
-        name = "option_name",
-        description = "Option description",
-        required = True,
-        opt_type = interactions.OptionType.STRING
-    )
-    async def module_group_ping(self, ctx: interactions.SlashContext, option_name: str):
-        await ctx.send(f"Pong {option_name}!")
-        internal_t.internal_t_testfunc()
-
-    @module_base.subcommand("pong", sub_cmd_description="Replace the description of this command")
-    @interactions.slash_option(
-        name = "option_name",
-        description = "Option description",
-        required = True,
-        opt_type = interactions.OptionType.STRING
-    )
-    async def module_group_pong(self, ctx: interactions.SlashContext, option_name: str):
-        await ctx.send(f"Pong {option_name}!")
-        internal_t.internal_t_testfunc()
+    @module_group.subcommand("top", sub_cmd_description="回到顶楼")
+    
+    async def module_group_ping(self, ctx: interactions.SlashContext):
+        mess=ctx.message.jump_url
+        elev=mess.rsplit('/', 1)[0]
+        await ctx.send(elev+'/0')
+        
